d926cd93dd50f082f0f5b624f4f62420d9491df9
MahmoudMuhammed92 <mahmoud.roxy@gmail.com>
2018-05-09 00:48:31 +0300

    add login&signup

---
+++ app/src/main/java/com/example/william/reconnect/LoginActivity.java
@@ -0,0 +1,187 @@
+package com.example.william.reconnect;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.william.reconnect.activities.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.progressbar)
+    ProgressBar progressbar;
+    private FirebaseAuth mAuth;
+
+
+    @BindView(R.id.logo)
+    ImageView logo;
+    @BindView(R.id.editTextEmail)
+    EditText editTextEmail;
+    @BindView(R.id.editTextPassword)
+    EditText editTextPassword;
+    @BindView(R.id.login_btn)
+    Button loginBtn;
+    @BindView(R.id.signup_btn)
+    Button signupBtn;
+    @BindView(R.id.forgot)
+    TextView forgot;
+    @BindView(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+    private void registerUser() {
+
+        String userMail = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+
+        if (userMail.isEmpty()) {
+
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(userMail).matches()) {
+            editTextEmail.setError("Please enter a valid Email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is requried!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Should be 6 or more!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressbar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(userMail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressbar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Register Successful!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+                else {
+
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException){
+
+                        Toast.makeText(LoginActivity.this, "Email is registered!", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+
+                        Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+
+    }
+
+
+    private void loginUser(){
+
+        String userMail = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+
+        if (userMail.isEmpty()) {
+
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(userMail).matches()) {
+            editTextEmail.setError("Please enter a valid Email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is requried!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Should be 6 or more!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressbar.setVisibility(View.VISIBLE);
+
+
+        mAuth.signInWithEmailAndPassword(userMail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressbar.setVisibility(View.GONE);
+                if (task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                }
+                else {
+                    progressbar.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @OnClick({R.id.login_btn, R.id.signup_btn})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.login_btn:
+                loginUser();
+                break;
+            case R.id.signup_btn:
+                registerUser();
+                break;
+        }
+    }
+}
