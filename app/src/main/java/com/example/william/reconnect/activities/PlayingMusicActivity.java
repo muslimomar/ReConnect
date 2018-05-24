f08855bb6a1ba6628b76b992c7ffcace9be15819
MahmoudMuhammed92 <mahmoud.roxy@gmail.com>
2018-05-25 01:59:32 +0300

    playing music full finished

--- app/src/main/java/com/example/william/reconnect/activities/PlayingMusicActivity.java
+++ app/src/main/java/com/example/william/reconnect/activities/PlayingMusicActivity.java
@@ -1,7 +1,9 @@
 package com.example.william.reconnect.activities;
 
-import android.app.Dialog;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.RadialGradient;
 import android.graphics.Shader;
@@ -10,31 +12,41 @@ import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.PaintDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.william.reconnect.R;
 import com.example.william.reconnect.adapter.MusicListAdapter;
 import com.example.william.reconnect.model.Music;
+import com.example.william.reconnect.model.Reminder;
+import com.example.william.reconnect.model.SilenceModel;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.realm.Realm;
+
+import static com.example.william.reconnect.util.Extras.RANDOM;
 
 public class PlayingMusicActivity extends AppCompatActivity {
 
     Context context = this;
+    MediaPlayer player;
 
 
     @BindView(R.id.back_arrow_btn)
@@ -49,22 +61,113 @@ public class PlayingMusicActivity extends AppCompatActivity {
     ImageView musicBtn;
     @BindView(R.id.relative_layout)
     RelativeLayout relativeLayout;
-
-    MusicListAdapter mAdapter;
+    String musicType = "";
     ArrayList<Music> musicModels;
-
+    int ONE_MIN_MS = 60000;
+    private int[] rawRef = {R.raw.jason_shaw_acoustuc_meditation,R.raw.kevin_macleod_bathed_in_the_light,R.raw.kevin_macleod_dream_culture,R.raw.kevin_macleod_enchanted_journey,R.raw.kevin_macleod_meditation_impromptu,R.raw.kevin_macleod_smoother_move,R.raw.kevin_macleod_sovereign_quarter,R.raw.kevin_macleod_windswept,R.raw.lee_rosevere_betrayal,R.raw.lee_rosevere_everywhere,R.raw.lee_rosevere_not_my_problem,R.raw.ryan_andersen_day_to_night,R.raw.lee_rosevere_well_figure_it_out_together};
+    Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_playing);
         ButterKnife.bind(this);
+        realm = Realm.getDefaultInstance();
 
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            musicType = bundle.getString("music_type");
+        }
+
+         Random random = new Random();
+
+
+        switch (musicType){
+            case "Jason Shaw Acoustuc Meditation":player = MediaPlayer.create(this, R.raw.jason_shaw_acoustuc_meditation);
+                player.start();
+            case "Kevin MacLeod - Sovereign Quarter":player = MediaPlayer.create(this,R.raw.kevin_macleod_sovereign_quarter);
+                player.start();
+                break;
+            case "Kevin MacLeod_Dream_Culture":player = MediaPlayer.create(this,R.raw.kevin_macleod_dream_culture);
+            player.start();
+                break;
+            case "Kevin Macleod Bathed in The Light[Good for Chakra]":player = MediaPlayer.create(this,R.raw.kevin_macleod_bathed_in_the_light);
+            player.start();
+                break;
+            case "Kevin Macleod Windswept":player = MediaPlayer.create(this,R.raw.kevin_macleod_windswept);
+            player.start();
+                break;
+            case "Kevin MacLeod Enchanted Journey":player = MediaPlayer.create(this,R.raw.kevin_macleod_enchanted_journey);
+            player.start();
+                break;
+            case "Kevin MacLeod Smoother Move":player = MediaPlayer.create(this,R.raw.kevin_macleod_smoother_move);
+            player.start();
+                break;
+            case "Kevin MacLeod Meditation Impromptu":player = MediaPlayer.create(this,R.raw.kevin_macleod_meditation_impromptu);
+            player.start();
+                break;
+            case "Lee Rosevere Everywhere":player = MediaPlayer.create(this,R.raw.lee_rosevere_everywhere);
+            player.start();
+                break;
+            case "Lee Rosevere Betrayal":player = MediaPlayer.create(this,R.raw.lee_rosevere_betrayal);
+            player.start();
+                break;
+            case "Ryan Andersen Day to Night":player= MediaPlayer.create(this,R.raw.ryan_andersen_day_to_night);
+            player.start();
+                break;
+            case "Lee Rosevere We’ll figure it out together":player= MediaPlayer.create(this,R.raw.lee_rosevere_well_figure_it_out_together);
+            player.start();
+                break;
+            case "Lee Rosevere Not My Problem":player = MediaPlayer.create(this,R.raw.lee_rosevere_not_my_problem);
+            player.start();
+                break;
+           /* case "Random":player = MediaPlayer.create(this,rawRef[random.nextInt(rawRef.length)]);
+            player.start();
+            break;
+*/
+
+        }
+
+        Reminder reminder = realm.where(Reminder.class).findFirst();
+        if (reminder != null) {
+            // Get the timespent on Silence Day.
+            String musicType = reminder.getMusicPlaybackType();
+            if (musicType.equals("Random")){
+                player = MediaPlayer.create(this,rawRef[random.nextInt(rawRef.length)]);
+                player.start();
+            }
+        }
+
+     /*   if (musicType != null) {
+            if (musicType.equals(RANDOM)) {
+                Toast.makeText(context, musicType, Toast.LENGTH_SHORT).show();
+
+           }
+
+        }*/
 
         initializeImages();
         setImageOpacity();
         rotateChakra();
+        //playMusic();
+        Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                // TODO:  Stop music
+                    player.release();
+                    player = null;
+                playingIcon.clearAnimation();
+
+                if (!((Activity) PlayingMusicActivity.this).isFinishing()) {
+                    showFinishDialog();
+                }
+
+            }
+        };
+        handler.postDelayed(r, ONE_MIN_MS);
 
     }
+
 
     private void initializeImages() {
         playingIcon.setImageResource(R.drawable.re_connect_logo);
@@ -116,27 +219,41 @@ public class PlayingMusicActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.back_arrow_btn:
                 NavUtils.navigateUpFromSameTask(this);
+                player.stop();
                 break;
             case R.id.info_btn:
                 break;
             case R.id.music_btn:
-                Dialog dialog = new Dialog(this);
-                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                dialog.setCancelable(false);
-                dialog.setContentView(R.layout.music_list_dialog);
-                ListView musicList = (ListView) dialog.findViewById(R.id.music_list);
-
-                musicModels = new ArrayList<>();
-                musicModels.add(new Music("Ahmad kaya", "07:2"));
-                musicModels.add(new Music("Enrique Iglasias", "1:22"));
-                musicModels.add(new Music("Justin Beiber", "01:26"));
-                musicModels.add(new Music("Mahir Zain", "20:12"));
-                musicModels.add(new Music("Rihanna", "04:43"));
-                musicModels.add(new Music("Torn", "0:22"));
-                musicModels.add(new Music("Chab Khalid", "02:31"));
-                mAdapter = new MusicListAdapter(this, musicModels);
-                musicList.setAdapter(mAdapter);
-                dialog.show();
         }
     }
+
+    public void playMusic() {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.jason_shaw_acoustuc_meditation);
+
+        }
+        player.start();
+    }
+
+    private void showFinishDialog() {
+
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(PlayingMusicActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(PlayingMusicActivity.this);
+        }
+        builder.setTitle("Session Finished")
+                .setMessage("Your meditation session has finished!")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(PlayingMusicActivity.this, MainActivity.class));
+                        finish();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setCancelable(false)
+                .show();
+    }
+
 }
